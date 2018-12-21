6bc9d8efe4493c2f118d67cb97d9d91906264d13
th3eesome <610870693@qq.com>
2018-12-21 20:35:34 +0800

    advice api

--- router/patient.js
+++ router/patient.js
@@ -97,20 +97,22 @@ router.post('/oa/patients1/',async (ctx, next) =>{
 });
 
 //通过pid获取一附院病人病案首页信息
-router.get('/oa/patient1/:pid',async(ctx,next) => {
+router.get('/oa/patient1/:pid/:zyh',async(ctx,next) => {
+    console.log(ctx.params);
     let params = ctx.params;
-    let {pid} = params;
-    //console.log(ctx.params);
-    //let sql = `SELECT * FROM FIRST_HOME WHERE part1_pid=${pid}`;
-    await db.query(
-        'select * from FIRST_HOME where part1_pid= ?',
-        [pid]
-    ).then(res => {
-        Utils.cleanData(res);
-        ctx.body = {...Tips[0],data:res[0]}
+    let {pid, zyh} = params;
+    const home_sql = `select * from FIRST_HOME where part1_pid = ${pid}`;
+    const advice_sql = `select * from FIRST_ADVICE where part2_zyh = '${zyh}'`;
+    const home_part = await db.query(home_sql);
+    const advice_part = await db.query(advice_sql);
+    Promise.all([home_part, advice_part]).then(res => {
+        res[1].forEach(item => {
+            delete item['part2_pid'];
+        });
+        ctx.body = {...Tips[0], data: {home: res[0], advice: Utils.generateCategory(res[1], 'part2_yzlb')}}
     }).catch(e => {
-        ctx.body = {...Tips[1002],reason:e}
-    })
+        ctx.body = {...Tips[1002], reason:e}
+    });
 });
 
 module.exports = router;

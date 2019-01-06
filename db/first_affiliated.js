f1dab3ba5a69476723b7524cd291ffb2ae92b2c1
th3eesome <610870693@qq.com>
2019-01-06 21:34:39 +0800

    partxx与数据表对应关系在db目录下可查看

--- db/first_affiliated.js
+++ db/first_affiliated.js
@@ -1,19 +1,28 @@
 const type = {
+    // part1
     home_page_type : [
         'VARCHAR(60)', 'INT', 'VARCHAR(300)', 'VARCHAR(60)', 'INT', 'INT', 'VARCHAR(300)', 'VARCHAR(300)', 'VARCHAR(300)',
         'VARCHAR(100)', 'VARCHAR(100)', 'VARCHAR(100)', 'VARCHAR(100)', 'VARCHAR(100)', 'VARCHAR(60)', 'VARCHAR(60)', 'INT', 'VARCHAR(300)',
         'VARCHAR(100)', 'VARCHAR(60)', 'VARCHAR(300)', 'INT', 'VARCHAR(60)'
     ],
+
+    //part2
     advice_page_type : [
         'VARCHAR(60)', 'VARCHAR(60)', 'VARCHAR(150)', 'INT', 'DECIMAL(10 ,2)', 'VARCHAR(60)', 'VARCHAR(30)', 'VARCHAR(30)', 'VARCHAR(60)',
         'VARCHAR(30)', 'VARCHAR(60)', 'VARCHAR(60)', 'VARCHAR(60)', 'VARCHAR(60)'
     ],
+
+    //part3
     lis_page_type: [
         'VARCHAR(90)', 'INT', 'VARCHAR(60)', 'INT', 'VARCHAR(150)', 'VARCHAR(150)', 'VARCHAR(90)', 'VARCHAR(60)', 'VARCHAR(300)', 'VARCHAR(60)'
     ],
+
+    //part4
     operation_mazui_type: [
         'V6', 'V30', 'V12', 'V3', 'V3', 'V3', 'V3', 'V3', 'V3', 'V3', 'V3', 'V3', 'V3', 'V3'
     ],
+
+    //part5
     result_type: [
         'I', 'V9', 'V3', 'V30', 'V3', 'T', 'T'
     ],

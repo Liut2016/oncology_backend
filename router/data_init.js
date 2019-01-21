d8aabd89fd79ad777c9b1010d7029851de3c5e45
liutao <869044936@qq.com>
2019-01-21 14:44:08 +0800

    新增一附院API

--- router/data_init.js
+++ router/data_init.js
@@ -9,7 +9,6 @@ const iconv = require('iconv-lite');
 const Type = require('../db/first_affiliated');
 const second_Type = require('../db/second_affiliated');
 const Utils = require('../utils/methods');
-const _ = require('lodash');
 
 const type = Type.type;
 const sec_type = second_Type.type;
@@ -231,7 +230,7 @@ router.get('/oa/init_weight' ,async (ctx, next) => {
          return `${key.split(',').join('')}`;
      });
 
-     for (let i = 1; i<11; i++) {
+     for (let i = 1; i<6; i++) {
          const old_lis = fs.readFileSync(path.join(__dirname, `../data/first_data/first_lis/${i}.xlsx`));
          const old_lis_example = xlsx.parse(old_lis, {cellDates: true})[0].data.slice(1);
          const completed_data = Utils.completeRow(old_lis_example, 10, null);
@@ -716,45 +715,6 @@ router.get('/oa/generate_json_2', async(ctx, next) => {
     fs.writeFileSync(path.join(__dirname, '../generate/hos_2.json'), JSON.stringify(data));
     // 将此数组转换为键值-类型
 
-});
-
-router.get('/oa/generate_json_1', async (ctx, next) => {
-    const home_data = fs.readFileSync(path.join(__dirname, '../data/first_data/first_home_page.xls'));
-    const results_data = fs.readFileSync(path.join(__dirname, '../data/first_data/first_results/results.xlsx'));
-    const home_key = xlsx.parse(home_data)[0].data[0].slice(1);
-    const results_key = xlsx.parse(results_data)[0].data[0].slice(2);
-    const advice_buffer = Buffer.from(fs.readFileSync(path.join(__dirname, '../data/first_data/first_advice/advice1.csv') , {encoding: 'binary'}), 'binary');
-    let csv_file = iconv.decode(advice_buffer, 'GBK');
-    const options = {
-        delimiter: ',',
-        quote: '"'
-    };
-    const advice_key = Object.keys(csv.toObject(csv_file, options)[0]).slice(1);
-    const key_json = {};
-    const key_array = [home_key, advice_key, results_key];
-    const part_map = {
-        1: '病案首页',
-        2: '医嘱',
-        3: '影像报告'
-    };
-    key_array.forEach((item, index) => {
-        key_json[`part${index + 1}`] = {
-            'name': part_map[index + 1],
-            'items': key_array[index].map(key => {
-                const p_key = `part${index + 1}_${PY_translator(key, {style: PY_translator.STYLE_FIRST_LETTER})}`;
-                return {
-                    name: key,
-                    key: p_key.split(',').join(''),
-                    belong: part_map[index + 1],
-                    type: _.endsWith(key, '时间') ? 'date' : ''
-                }
-            })
-        };
-    });
-
-    ctx.body = {
-        key: key_json
-    }
 });
 
 generateType = (type) => {
--- router/patients_second.js
+++ router/patients_second.js
@@ -103,6 +103,7 @@ router.post('/oa/patients2/filter',async (ctx, next) =>{
     var where_array = [];
     var where = ''  ;
     var set = '';
+    console.log(conditions);
     conditions.forEach(item => {
         searchField.push(item.databaseField);
         logicValue.push(item.logicValue);
@@ -110,7 +111,9 @@ router.post('/oa/patients2/filter',async (ctx, next) =>{
             if(i === item.form_type){
                 formType.push(form[i]);
             }
-        });
+        })
+        console.log(formType);
+
           //字符型查找
           if ((item.isNotNumber === true) && (item.isSelect === false)) {
               if (item.selectedValue === '包含') {
@@ -138,6 +141,9 @@ router.post('/oa/patients2/filter',async (ctx, next) =>{
               where_array.push(`(${item.databaseField} between '${item.startTime}' and '${item.endTime}')`);
           }
     });
+    
+  
+    console.log(searchField);
     where_array.forEach((item, index) => {
           if ( index === where_array.length - 1) {
               where = ` ${where}${item} `;
@@ -146,32 +152,34 @@ router.post('/oa/patients2/filter',async (ctx, next) =>{
           }
     });
 
-
     formType.push('SECOND_HOME');
-    if(formType.indexOf('SECOND_FEE') !== -1){
+
+    if(formType.indexOf('SECOND_FEE')!=-1){
         where = `(part1_bah=part2_bah) and ${where}`;
     }
 
     
     let sql1;
     let sql2;
-    if((conditions.length !== 0)&&(isAll===false)) {
-        searchField.push('part1_pid', 'part1_bah', 'part1_xm', 'part1_ryzd', 'part1_rysj', 'part1_sjzyts');
+    if((conditions.length!=0)&&(isAll===false)){
+        searchField.push('part1_bah', 'part1_xm', 'part1_rysj', 'part1_ryzd' , 'part1_pid');
         sql1 = `SELECT ${unique(searchField)} FROM ${unique(formType)} where ${where} limit ${start},${pagesize};`;
         sql2 = `SELECT count(1) as num from (SELECT ${unique(searchField)}  FROM ${unique(formType)} where ${where}) as temp ;`;
         // sql2 = `SELECT ${unique(searchField)}, count(1) AS num FROM ${unique(formType)} where ${where} GROUP BY ${unique(searchField)};`;
     }else if((conditions.length!=0)&&(isAll===true)){
         sql1 = `SELECT ${unique(searchField)} FROM ${unique(formType)} where ${where} limit ${start},${pagesize};`;
-
         sql2 = `SELECT count(1) as num from (SELECT ${unique(searchField)}  FROM ${unique(formType)} where ${where}) as temp ;`;
     }else{
-        sql1 = `SELECT part1_pid,part1_bah, part1_xm,part1_ryzd,part1_rysj,part1_sjzyts FROM SECOND_HOME limit ${start},${pagesize};`;
+        sql1 = `SELECT part1_xm,part1_bah,part1_rysj,part1_ryzd,part1_pid FROM SECOND_HOME limit ${start},${pagesize};`
         sql2 = 'SELECT COUNT(*) FROM SECOND_HOME;'
     }
 
+   console.log(sql1);
+   console.log(sql2);
     const part1 = await db.query(sql1);
     const part2 = await db.query(sql2);
     Promise.all([part1, part2]).then((res) => {
+        console.log(res);
         data = res[0];
         data.forEach(element => {
                 Object.keys(element).forEach( item=>{
@@ -182,15 +190,18 @@ router.post('/oa/patients2/filter',async (ctx, next) =>{
                         } 
                         if(element[item]===2){
                             element[item]='女';
-                        }
+                        };
                     }
                 })
-             });
-        if(conditions.length !== 0){
+             })
+        if(conditions.length!=0){
             num = res[1][0]['num'];
         }else{
             num = res[1][0]['COUNT(*)'];
         }
+        console.log(num);
+        
+        //Utils.cleanData(res);
         ctx.body = {...Tips[0],count_num:num,data:data};
         // ctx.body = {...Tips[0],data:data};
 
@@ -390,7 +401,7 @@ router.post('/oa/patients2/dim',async(ctx,next) => {
         res.forEach(function(element){
             //console.log(element[`${dim}`]);
             result.push(element[`${dim}`]);
-        });
+        })
         ctx.body = {...Tips[0],dim:result};
     }).catch((e) => {
         ctx.body = {...Tips[1002],reason:e};
